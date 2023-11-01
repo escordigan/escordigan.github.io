e1e97bcbb286544f9ea88f94594eb6b7bb56cba6
Elena Vargas <149540525+escordigan@users.noreply.github.com>
2023-10-31 21:09:32 -0600

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,46 @@
+print("""Hi! Thank you for being here.
+Let's have a lot of fun together!
+
+      """)
+food_name= input("Enter food name: ")
+room_house_1= input("Room in the house # 1: ")
+room_house_2= input("Room in the house # 2: ")
+noun_1= input("Noun # 1: ")
+noun_2= input("Noun # 2: ")
+noun_3= input("Noun # 3: ")
+noun_4= input("Noun # 4: ")
+noun_5= input("Noun # 5: ")
+adjective_1= input("Adjective # 1: ")
+adjective_2= input("Adjective # 2: ")
+person_name_1= input("Person's name # 1: ")
+person_name_2= input("Person's name # 2: ")
+person_name_3= input("Person's name # 3: ")
+verb= input("Verb: ")
+animal= input("Animal: ")
+
+print("\n")
+ 
+
+print("HERE IS YOUR STORY:"
+
+            "\n")                              #below use of " " for word spaces#
+
+story= ("Last week was my birthday. We went to my grandma's house and had" " " + food_name + " " "for supper." "\n"
+        "After that, we all sat in the" " " + room_house_1 + " " "to open my presents. The first gift was from my sister, and it was a" " " + noun_1 + "." " "
+        "The next gift was from my mom and it was a(n)" " " + adjective_1 + " " + noun_2 + "." "\n"
+        "The gift after that was a" " " + noun_3 + ". I pretended to be happy, but I already had one of those!" "\n"
+        "All of a sudden, I heard a strange noise. The noise was" " " + adjective_2 + " " "and I couldn’t tell where it was coming from." "\n"
+        "I looked around the room. I stood up and walked really fast to the" " " + room_house_2 + ". I looked in the" " " + noun_4 + ". I look under the" " " + noun_5 + ". I found nothing!" "\n"
+        "I realized that everyone in the room was laughing! I asked my friend" " " + person_name_1 + ": ‘What’s going on?’ I asked my other friend," " "
+        + person_name_2 + ". They just laughed. I asked" " " + person_name_3 + " " "but they just " + verb + " " "around the room." "\n"
+        "I heard the noise again. Suddenly, it occurred to me the noise was coming from one of the presents! I opened the gift and inside the box was a baby" " " + animal + "! What a surprise!")
+
+       
+
+print(story)
+
+#Madlibs story was taken from website: https://www.parenttrust.org/2019/12/02/mad-libs/#
+
+
+
+
